2f0861fa461d63dfbc871d8d7dc91760b1db28af
leroosje <leroosje@uncg.edu>
2019-11-20 21:34:52 -0500

    Added special csv for test data

---
+++ CSVGenerator_SpecialFeature_Test.py
@@ -0,0 +1,27 @@
+import csv
+
+i = 1
+file = open("WinogradChallenge.txt", "r")
+formattedfile = file.read().splitlines()
+
+with open("WinogradSpecialFeatureCSV.csv", "w", encoding='UTF8') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(["Question String", "Pronoun", "Split1", "Split2"])
+    for line in formattedfile:
+        if i % 7 == 1:
+            question = line
+        if i % 7 == 2:
+            snippet = line[9:]
+        if i % 7 == 0: #Totally arbitrary condition to print questions.
+            print(question)
+            print(snippet)
+            split1, split2 = question.split(" " + snippet, 1)
+
+            row1 = [question, snippet, split1, snippet + split2]
+
+            writer.writerow(row1)
+        i += 1
+
+
+
+
