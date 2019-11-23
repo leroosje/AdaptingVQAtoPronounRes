36b4f407450062e0ac3ddd2d04285fae182a0ce3
leroosje <leroosje@uncg.edu>
2019-11-22 21:43:30 -0500

    Added two columns to special features CSVs

--- CSVGenerator_SpecialFeature.py
+++ CSVGenerator_SpecialFeature.py
@@ -6,17 +6,19 @@ formattedfile = file.read().splitlines()
 
 with open("UTDallasSpecialFeatureCSV.csv", "w", encoding='UTF8') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(["Question String", "Pronoun", "Split1", "Split2"])
+    writer.writerow(["Question String", "Pronoun", "Split1", "Split2", "Option1", "Option2"])
     for line in formattedfile:
         if i % 5 == 1:
             question = line
         if i % 5 == 2:
             pronoun = line
+        if i % 5 == 3:
+            answerchoices = line
         if i % 5 == 0: #Totally arbitrary condition to print questions.
 
             split1, split2 = question.split(" " + pronoun, 1)
-
-            row1 = [question, pronoun, split1, pronoun + split2]
+            answer1, answer2 = answerchoices.split(",")
+            row1 = [question, pronoun, split1, pronoun + split2, answer1, answer2]
 
             writer.writerow(row1)
         i += 1
--- CSVGenerator_SpecialFeature_Test.py
+++ CSVGenerator_SpecialFeature_Test.py
@@ -6,18 +6,22 @@ formattedfile = file.read().splitlines()
 
 with open("WinogradSpecialFeatureCSV.csv", "w", encoding='UTF8') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(["Question String", "Pronoun", "Split1", "Split2"])
+    writer.writerow(["Question String", "Pronoun", "Split1", "Split2", "Option1", "Option2"])
     for line in formattedfile:
         if i % 7 == 1:
             question = line
         if i % 7 == 2:
             snippet = line[9:]
+        if i % 7 == 3:
+            answer1 = line
+        if i % 7 == 4:
+            answer2 = line
         if i % 7 == 0: #Totally arbitrary condition to print questions.
             print(question)
             print(snippet)
             split1, split2 = question.split(" " + snippet, 1)
 
-            row1 = [question, snippet, split1, snippet + split2]
+            row1 = [question, snippet, split1, snippet + split2, answer1, answer2]
 
             writer.writerow(row1)
         i += 1
