4181f038eca33769d4c40c216a9637559b64c5ce
leroosje <leroosje@uncg.edu>
2019-12-07 16:18:11 -0500

    Cleaned up scripts for submission

--- GenderFeatureCSVGenerator.py
+++ GenderFeatureCSVGenerator.py
@@ -1,14 +1,3 @@
-#NOTES:
-#This script generates question from the UT-Dallas Dataset.
-#It makes line specific adjustments to generate the correct data; it will NOT work on general datasets
-#Given a sentence, it will generate two questions:
-#--------------"What does <pronoun> refer to?"
-#--------------"What <clause>?"
-#--------------FOR EXAMPLE: "The bee landed on the flower because it had pollen."
-#--------------"What does it refer to?"
-#--------------"What had pollen?"
-#These question will be output to a text file, with each question on a seperate line.
-#No white space between question pairs.
 import csv
 import random
 from nltk.corpus import names
